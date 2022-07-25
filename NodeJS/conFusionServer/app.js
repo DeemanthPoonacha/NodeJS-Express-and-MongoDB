b6116c9aa2e5962ca2a1e81891c7593fe33238a7
DeemanthKP <deemanth.p@tataelxsi.co.in>
2022-07-25 19:21:25 +0530

    Rest with express and mangoose

--- NodeJS/conFusionServer/app.js
+++ NodeJS/conFusionServer/app.js
@@ -4,11 +4,21 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const mongoose = require('mongoose');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
+
+const Dishes = require('./models/dishes');
+
+const url='mongodb+srv://user01:user01@cluster0.gkrfc.mongodb.net/test'
+const connect = mongoose.connect(url);
+connect.then((db) => {
+  console.log("Connected correctly to server");
+}, (err) => { console.log(err) });
 
 var app = express();
 
