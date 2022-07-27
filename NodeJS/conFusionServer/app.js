1de689aa0ea958204c34ef58c5821ddb3cdb4291
DeemanthKP <deemanth.p@tataelxsi.co.in>
2022-07-27 19:33:43 +0530

    Upload files

--- NodeJS/conFusionServer/app.js
+++ NodeJS/conFusionServer/app.js
@@ -17,6 +17,9 @@ var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
 
+const uploadRouter = require('./routes/uploadRouter');
+
+
 const mongoose = require('mongoose');
 
 const Dishes = require('./models/dishes');
@@ -58,6 +61,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes', dishRouter);
 app.use('/promotions',promoRouter);
 app.use('/leaders',leaderRouter);
+
+app.use('/imageUpload', uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
