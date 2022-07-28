63d1fa8b75245175838dea4b8c0d9236ed5fe93a
DeemanthKP <deemanth.p@tataelxsi.co.in>
2022-07-28 13:13:47 +0530

    Facebook Oauth

--- NodeJS/conFusionServer/config.js
+++ NodeJS/conFusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb+srv://user01:user01@cluster0.gkrfc.mongodb.net/test'
+    'mongoUrl' : 'mongodb+srv://user01:user01@cluster0.gkrfc.mongodb.net/test',
+    'facebook': {
+        clientId: '424807673040290',
+        clientSecret: '684138b1cc11ff08aa768075cb57c37a'
+    }
 }
