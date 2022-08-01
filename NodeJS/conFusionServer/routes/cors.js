61b5d60e5adc4257901bd0eaa45d5396e4ef8500
DeemanthKP <deemanth.p@tataelxsi.co.in>
2022-08-01 13:57:54 +0530

    Working Server

--- NodeJS/conFusionServer/routes/cors.js
+++ NodeJS/conFusionServer/routes/cors.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-const whitelist = ['http://localhost:3000', 'https://localhost:3443'];
+const whitelist = ['http://localhost:3000', 'https://localhost:3443','http://24069-440238.TELXSIDS.NET:3001'];
 var corsOptionsDelegate = (req, callback) => {
     var corsOptions;
     console.log(req.header('Origin'));
